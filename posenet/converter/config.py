44189f620f32cbbb55c47498638acddc96ff0771
Markus Matiaschek <mmatiaschek@gmail.com>
2019-06-17 16:15:30 +0200

    Revert "Posnet added"
    This reverts commit 171b2bee5f2bfdce81c9f81dfb6fa00b345ebd71.

--- posenet/converter/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import os
-
-
-
-def load_config(config_name='config.yaml'):
-    cfg_f = open(os.path.join('/home/prajwal/cgm-ml/posenet/converter', config_name), "r+")
-    cfg = yaml.load(cfg_f)
-    return cfg
