f406857949e5da957089cb983dd5ea883dbc013b
Markus Hinsche <m.hinsche@gmail.com>
2020-10-15 18:28:28 +0200

    Document beheading technique

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifactlatefusion-plaincnn-height/src/model.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifactlatefusion-plaincnn-height/src/model.py
@@ -5,7 +5,7 @@ def load_base_cgm_model(model_fpath, should_freeze=False):
     # load model
     loaded_model = models.load_model(model_fpath)
 
-    # cut off last layer
+    # cut off last layer (https://stackoverflow.com/a/59304656/5497962)
     beheaded_model = models.Sequential()
     for layer in loaded_model.layers[:-1]:
         beheaded_model.add(layer)
