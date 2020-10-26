a20bba94253115c101aae83b4120e3d17fa1627e
whyshreddy <vaishnavireddy.gutha93@gmail.com>
2020-10-26 16:24:11 +0000

    autopep8 aggresively

--- src/common/rgbd_toolkit/cgm_fusion/test_viz_fusion.py
+++ src/common/rgbd_toolkit/cgm_fusion/test_viz_fusion.py
@@ -1,11 +1,10 @@
+import numpy
+from cgm_fusion import utility
+from cgm_fusion import fusion
+from cgm_fusion import calibration
 import sys
 sys.path.insert(0, "..")
 
-from cgm_fusion import calibration
-from cgm_fusion import fusion
-from cgm_fusion import utility
-
-import numpy
 
 utility.get_viz_depth("/tmp/cloud_debug.ply")
 utility.get_viz_rgb("/tmp/cloud_debug.ply")
