d1146505bc476f353d8cd721b81d44e736a9e21b
whyshreddy <vaishnavireddy.gutha93@gmail.com>
2020-10-27 06:40:51 +0000

    more clean up

--- src/common/rgbd_toolkit/README.md
+++ src/common/rgbd_toolkit/README.md
@@ -2,11 +2,43 @@
 **RGBD Data Creation**
 ---
 
-## Steps
+1.  Make sure input path is a list of qr codes. Each qr code should contain a folder of pcd files and a folder of rgb files. Mention an output path where you want the RGBD data to be created.
 
-1.  Make sure input path is a list of qr codes. Mention an output path where you want the RGBD data to be created.
-2. usage: rgbd.py [-h] [--input inputpath] [--output outputpath] optional: [--mounted] if you are using the input as the mounted datastore of qr codes.
-3. #TODO add segmented rgbd
+input dir tree structure should be:
+```
+qrcode
+│      
+│
+└───qrcode1
+│   │   
+│   │
+│   └───pc
+│   │    │__.*pc 
+│   │     
+│   └───rgb 
+│        |__.*png
+│   
+└───qrcode2
+|    │   
+│   │
+│   └───pc
+│   │    │__.*pc 
+│   │     
+│   └───rgb 
+│        |__.*png
+```
+
+2. usage: 
+```
+rgbd.py [-h] [--input inputpath] [--output outputpath] optional: [--mounted if using the input as the mounted datastore of qr codes]  [--w specifying number of workers]
+```
+
+For eg: 
+```
+python rgbd.py --input [/mnt/cgmmlprod/cgminbmzprod_v5.0/] --output [/mnt/preprocessed/rgbd56k/] --mounted --w [20]
+```
+
+3. TODO add segmented rgbd
 
 
 
--- src/common/rgbd_toolkit/rgbd.py
+++ src/common/rgbd_toolkit/rgbd.py
@@ -1,9 +1,6 @@
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-
 from PIL import Image
-
-#import cProfile
 import azureml.core
 from azureml.core.dataset import Dataset
 import json
@@ -15,29 +12,20 @@ from pathlib import Path
 print("Azure ML SDK Version: ", azureml.core.VERSION)
 import sys
 sys.path.append('../cgm-ml')
-sys.path.append('../cgm-ml/src/data_utils/dataset_EDA/50k_pcd')
-#print("sys.path",sys.path)
 sys.path.append(os.path.dirname(os.getcwd()))
-
 import numpy as np
 import pickle
 from numpy import size
 import pandas as pd
 from tqdm import tqdm
-
 from segmentation import DeepLabModel,load_model,apply_segmentation
 from get_timestamps import get_timestamps_from_rgb,get_timestamps_from_pcd
 import cgm_fusion.utility as utility
-from cgm_fusion.utility import Channel
 import cgm_fusion.calibration 
 from cgm_fusion.fusion import apply_fusion,fuse_rgbd 
-
-
 # import core packages from cgm
 from cgmcore.utils import load_pcd_as_ndarray
 from cgmcore import  utils
-
-
 import datetime
 import matplotlib.pyplot as plt
 from io import BytesIO
@@ -46,12 +34,9 @@ import tempfile
 from six.moves import urllib
 import warnings
 warnings.filterwarnings("ignore")
-
-
 import logging
 logging.getLogger('').handlers = []
 logging.basicConfig(filename='./RGBD.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
-
 import argparse
 
  
@@ -77,17 +62,18 @@ def process_pcd(norm_pcd_time,process_index=0):
             logging.info("timestamp of pcd: " + "{0:.2f}".format(round(pcd,2))+ " with index " + str(i)  + " path: " + str(pcd_path[i]))
             logging.info("timestamp of jpg: " + "{0:.2f}".format(round(norm_rgb_time[nn],2)) + " with index " + str(nn) + " path: " + str(rgb_path[nn]))
             
-            # get the original file path 
+            #get the original file path 
             path, filename = os.path.split(str(pcd_path[i]))
 
+            #located the nearest jpg file of that pcd
             pcd_file = pcd_path[i]
             jpg_file = rgb_path[nn]
             
-            
+            #rotating and aligning rgb image
             img_name=jpg_file.split("/")[-1]
             img=Image.open(jpg_file)
     
-            #rotating and aligning rgb image
+            
             if '_100_' in img_name or '_101_' in img_name or '_102_' in img_name:
                 image = img.rotate(angle=270)
                 
@@ -134,7 +120,7 @@ def process_pcd(norm_pcd_time,process_index=0):
             pc_filename = pcd_path_old.replace(".pcd", ".ply")
             pc_filename = pc_filename.replace("pc_",   "pcrgb_")
             
-            
+            #make a folder for scan type
             qr_rgbd=os.path.join(rgbd_folder_,qr_folder)
             qr_rgbd_scan=os.path.join(qr_rgbd,str(scan_type))
             
@@ -143,7 +129,7 @@ def process_pcd(norm_pcd_time,process_index=0):
                 height=int(artifacts_file.loc[np.where(artifacts_file["qrcode"]==qr_folder)].iloc[0].loc["height"])
                 weight=int(artifacts_file.loc[np.where(artifacts_file["qrcode"]==qr_folder)].iloc[0].loc["weight"])
             
-            
+            #make the output rgbd filename
             rgbd_filename = pc_filename.replace(fused_folder.split("measure")[0], qr_rgbd_scan+"/")
             
     
@@ -151,7 +137,7 @@ def process_pcd(norm_pcd_time,process_index=0):
             rgbd_filename=rgbd_filename.replace(".ply",".pkl")
             rgbd_filename=rgbd_filename.replace("/pc/","")  
             
-        
+            #check if output rgbd folder exists
             rgbd_folder=os.path.dirname(rgbd_filename)
             if not(os.path.isfile(rgbd_folder)): 
                 logging.info("Folder does not exist for " + str(rgbd_filename))
@@ -161,23 +147,30 @@ def process_pcd(norm_pcd_time,process_index=0):
 
             logging.info("Going to writing new fused data to: " + rgbd_filename)
 
-            #checking if file already exists and saving the rgbd file
-            if args.mounted:
-                if not os.path.exists(rgbd_filename):
-                    try: 
-                        rgbdseg_arr = fuse_rgbd(calibration_file,pcd_file, image)#, seg_path)
+            #checking if file already exists and saving the rgbd file with labels if its mounted data
+            if not os.path.exists(rgbd_filename):
+                try: 
+                    rgbdseg_arr = fuse_rgbd(calibration_file,pcd_file, image)#, seg_path)
+                    if args.mounted:
                         labels=np.array([height,weight])
                         data=(rgbdseg_arr,labels)
-                        pickle.dump(data, open(rgbd_filename, "wb")
-                        
-                        logging.info("successfully wrote new data to" + rgbd_filename)                              
-                    except Exception as e: 
-                        logging.error("Something went wrong.Skipping this file" + pc_filename)
-                        
-                        logging.error(str(e))
-                        continue
-            else:
-                np.save(rgbd_filename,rgbdseg_arr)
+                        pickle.dump(data, open(rgbd_filename, "wb"))
+                    else:
+                    #saving as a png file if not mounted data
+                        rgbd_filename=rgbd_filename.replace(".pkl",".png")
+                        fig = plt.figure()
+                        plt.imsave(rgbd_filename,rgbdseg_arr)
+                        #np.save(rgbd_filename,rgbdseg_arr)
+
+                    logging.info("successfully wrote new data to" + rgbd_filename)                              
+                except Exception as e: 
+                    logging.error("Something went wrong.Skipping this file" + pc_filename)
+                    
+                    logging.error(str(e))
+                    continue
+            
+            
+                
                 
 
 
@@ -193,7 +186,7 @@ if __name__ == "__main__":
                        metavar='outputpath',default="./output_rgbd",
                        type=str,
                        help='output rgbd folder path')
-    parser.add_argument("--w",metavar="workers",default=int(4),help="no. of cpu workers you want to process with")
+    parser.add_argument("--w",metavar="workers",type=int,default=None,help="no. of cpu workers you want to process with")
     parser.add_argument("--mounted",action='store_true',help="if you are processing on mounted data of a datastore")
     parser.add_argument("--segmented",action='store_true',help="if you want fused rgbd with segmentation")
     args = parser.parse_args()
@@ -203,15 +196,22 @@ if __name__ == "__main__":
 
     #reading artifacts.csv for mounted qrcode paths
     if args.mounted:
+        artifacts=os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),'data_utils/dataset_EDA/50k_pcd/artifacts.csv')
         mnt=args.input+"/qrcode/"
-        artifacts_file=pd.read_csv("artifacts.csv")
+        artifacts_file=pd.read_csv(artifacts)
         qrcode=artifacts_file["qrcode"]
         paths=artifacts_file["storage_path"]
         unique_qr_codes=[os.path.join(mnt,line) for line in qrcode]
+        
     else:
         folders=os.listdir(args.input)
         unique_qr_codes=[os.path.join(args.input,x) for x in folders]
-
+    
+    ##TODO: streamline validation of input directory
+    if not os.path.exists(unique_qr_codes[0]):
+        print("Error:invalid input paths..exiting")
+        exit()
+        
     #loading DeepLab model
     if args.segmented:
         model=load_model()
@@ -223,7 +223,7 @@ if __name__ == "__main__":
         os.mkdir(rgbd_folder_)
 
 
-   
+    #initialize empty lists for rgb and pcd paths of a given qr code
     rgb_paths=[]
     pcd_paths=[]
 
@@ -240,8 +240,8 @@ if __name__ == "__main__":
                         if file.endswith(".pcd"):
                             pcd_paths.append(dir_path)
         
-                            
-        
+            
+    #getting the timestamps of rgb and pcd paths
         [norm_rgb_time, rgb_path] = get_timestamps_from_rgb(rgb_paths)
         [norm_pcd_time, pcd_path] = get_timestamps_from_pcd(pcd_paths)
 
@@ -256,16 +256,16 @@ if __name__ == "__main__":
             continue
 
 
-
+    #processing every pcd file with its nearest rgb using multiprocessing workers
         utils.multiprocess(norm_pcd_time,
         process_method = process_pcd, 
         process_individial_entries  = False, 
         number_of_workers           = args.w,
         pass_process_index          = True, 
         progressbar                 = True, 
-        disable_gpu                 =False)
+        disable_gpu                 =True)
         
-    
+        
     end = datetime.datetime.now()
     diff = end - start
     print("***Done***")
