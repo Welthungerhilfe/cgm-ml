9ddcfdc1bf1377fba0c42f5a1d1963471d3e97c5
gangooteli <24511086+gangooteli@users.noreply.github.com>
2020-11-04 13:20:37 +0000

    pepify the code to fix broken build

--- src/common/eval/QA/eval-depthmap-height/src/test_config.py
+++ src/common/eval/QA/eval-depthmap-height/src/test_config.py
@@ -7,56 +7,56 @@ class dotdict(dict):
 
 #Details of model used for evaluation
 MODEL_CONFIG = dotdict(dict(
-    EXPERIMENT_NAME = 'q3-depthmap-plaincnn-height-95k',
-    
-    RUN_ID = 'q3-depthmap-plaincnn-height-95k_1597988908_42c4ef33',    #Run 3
+    EXPERIMENT_NAME='q3-depthmap-plaincnn-height-95k',
+
+    RUN_ID='q3-depthmap-plaincnn-height-95k_1597988908_42c4ef33',  # Run 3
     #RUN_ID = 'q3-depthmap-plaincnn-height-95k_1600451633_cb44f6db',     #Run 17
-    
-    INPUT_LOCATION = 'outputs',
-    NAME = 'best_model.h5',
+
+    INPUT_LOCATION='outputs',
+    NAME='best_model.h5',
 ))
 
 
 EVAL_CONFIG = dotdict(dict(
     #Name of evaluation
-    NAME = 'q3-depthmap-plaincnn-height-100-95k-run_17',
+    NAME='q3-depthmap-plaincnn-height-100-95k-run_17',
 
-    #Experiment in Azure ML which will be used for evaluation 
-    EXPERIMENT_NAME = "QA-pipeline",
-    CLUSTER_NAME = "gpu-cluster",
+    #Experiment in Azure ML which will be used for evaluation
+    EXPERIMENT_NAME="QA-pipeline",
+    CLUSTER_NAME="gpu-cluster",
 
     #Used for Debug the QA pipeline
-    DEBUG_RUN = False,
+    DEBUG_RUN=False,
     #DEBUG_RUN = True,
 
     #Will run eval on specified # of scan instead of full dataset
-    DEBUG_NUMBER_OF_SCAN = 50,
+    DEBUG_NUMBER_OF_SCAN=50,
 
-    SPLIT_SEED = 0,
+    SPLIT_SEED=0,
 ))
 
 #Details of Evaluation Dataset
 DATA_CONFIG = dotdict(dict(
     #Name of evaluation dataset
-    NAME = 'anon-depthmap-testset',
-    
-    IMAGE_TARGET_HEIGHT = 240,
-    IMAGE_TARGET_WIDTH = 180,
-    
-    #Batch size for evaluation
-    BATCH_SIZE = 256,
-    NORMALIZATION_VALUE = 7.5,
-    
-    # Parameters for dataset generation.
-    TARGET_INDEXES = [0],  # 0 is height, 1 is weight.
+    NAME='anon-depthmap-testset',
 
-    CODE_TO_SCANTYPE = {
-        '100' : '_front',
-        '101' : '_360',
-        '102' : '_back',
-        '200' : '_lyingfront',
-        '201' : '_lyingrot',
-        '202' : '_lyingback',
+    IMAGE_TARGET_HEIGHT=240,
+    IMAGE_TARGET_WIDTH=180,
+
+    #Batch size for evaluation
+    BATCH_SIZE=256,
+    NORMALIZATION_VALUE=7.5,
+
+    # Parameters for dataset generation.
+    TARGET_INDEXES=[0],  # 0 is height, 1 is weight.
+
+    CODE_TO_SCANTYPE={
+        '100': '_front',
+        '101': '_360',
+        '102': '_back',
+        '200': '_lyingfront',
+        '201': '_lyingrot',
+        '202': '_lyingback',
     }
 ))
 
@@ -65,9 +65,9 @@ DATA_CONFIG = dotdict(dict(
 RESULT_CONFIG = dotdict(dict(
     # Error margin on various ranges
     #EVALUATION_ACCURACIES = [.2, .4, .8, 1.2, 2., 2.5, 3., 4., 5., 6.]
-    ACCURACIES = [.2, .4, .6, 1,  1.2, 2., 2.5, 3., 4., 5., 6.],
-    COLUMNS = ['qrcode', 'artifact', 'scantype', 'GT', 'predicted'],
-    
+    ACCURACIES=[.2, .4, .6, 1, 1.2, 2., 2.5, 3., 4., 5., 6.],
+    COLUMNS=['qrcode', 'artifact', 'scantype', 'GT', 'predicted'],
+
     #path of csv file in the experiment which final result is stored
-    SAVE_PATH = './outputs/result.csv',
+    SAVE_PATH='./outputs/result.csv',
 ))
