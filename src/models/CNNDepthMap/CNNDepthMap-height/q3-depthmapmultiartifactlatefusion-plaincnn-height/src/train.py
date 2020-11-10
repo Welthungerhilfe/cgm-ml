c574fc129ffede399b36d2558efa86b5a2eec3b1
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2020-11-10 14:10:08 +0100

    Adjust latefusion experiment so it uses the common/model_utils/preprocessing.py code

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifactlatefusion-plaincnn-height/src/train.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifactlatefusion-plaincnn-height/src/train.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import random
+import shutil
 
 import glob2 as glob
 import tensorflow as tf
@@ -11,15 +12,29 @@ from tensorflow.keras import callbacks, layers, models
 from config import CONFIG, DATASET_MODE_DOWNLOAD, DATASET_MODE_MOUNT
 from constants import DATA_DIR_ONLINE_RUN, MODEL_CKPT_FILENAME, REPO_DIR
 from model import create_head, get_base_model
-from preprocessing import create_samples, tf_load_pickle, tf_augment_sample
+
+# Get the current run.
+run = Run.get_context()
+
+if run.id.startswith("OfflineRun"):
+    utils_dir_path = REPO_DIR / "src/common/model_utils"
+    utils_paths = glob.glob(os.path.join(utils_dir_path, "*.py"))
+    temp_model_util_dir = Path(__file__).parent / "tmp_model_util"
+    # Remove old temp_path
+    if os.path.exists(temp_model_util_dir):
+        shutil.rmtree(temp_model_util_dir)
+    # Copy
+    os.mkdir(temp_model_util_dir)
+    os.system(f'touch {temp_model_util_dir}/__init__.py')
+    for p in utils_paths:
+        shutil.copy(p, temp_model_util_dir)
+
+from tmp_model_util.preprocessing import create_samples, tf_load_pickle, tf_augment_sample
 from utils import download_dataset, get_dataset_path, AzureLogCallback, create_tensorboard_callback
 
 # Make experiment reproducible
 tf.random.set_seed(CONFIG.SPLIT_SEED)
 random.seed(CONFIG.SPLIT_SEED)
-
-# Get the current run.
-run = Run.get_context()
 
 DATA_DIR = REPO_DIR / 'data' if run.id.startswith("OfflineRun") else Path(".")
 print(f"DATA_DIR: {DATA_DIR}")
