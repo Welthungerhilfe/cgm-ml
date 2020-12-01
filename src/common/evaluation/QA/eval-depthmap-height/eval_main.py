908da56bcabbf944df6c1009b091df421923d2c1
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2020-12-01 15:38:53 +0100

    Put into main section

--- src/common/evaluation/QA/eval-depthmap-height/eval_main.py
+++ src/common/evaluation/QA/eval-depthmap-height/eval_main.py
@@ -18,110 +18,110 @@ from src.utils import download_model
 
 cwd = Path(__file__).parent
 
-code_dir = cwd / "src"
-paths = glob.glob(os.path.join(code_dir, "*.py"))
-print("paths:", paths)
+if __name__ == "__main__":
 
-#create a temp folder and copy code, model and dataset
+    # Create a temp folder
+    code_dir = cwd / "src"
+    paths = glob.glob(os.path.join(code_dir, "*.py"))
+    print("paths:", paths)
+    print("Creating temp folder...")
+    temp_path = cwd / "tmp_eval"
+    if os.path.exists(temp_path):
+        shutil.rmtree(temp_path)
+    os.mkdir(temp_path)
+    for p in paths:
+        shutil.copy(p, temp_path)
+    print("Done.")
 
-print("Creating temp folder...")
-temp_path = cwd / "tmp_eval"
-if os.path.exists(temp_path):
-    shutil.rmtree(temp_path)
-os.mkdir(temp_path)
+    auth = None if Run.get_context().id.startswith("OfflineRun") else get_auth()
+    print("auth:", auth)
+    ws = Workspace.from_config(auth=auth)
 
-for p in paths:
-    shutil.copy(p, temp_path)
-print("Done.")
+    # Copy model to temp folder
+    download_model(ws=ws,
+                experiment_name=MODEL_CONFIG.EXPERIMENT_NAME,
+                run_id=MODEL_CONFIG.RUN_ID,
+                input_location=os.path.join(MODEL_CONFIG.INPUT_LOCATION, MODEL_CONFIG.NAME),
+                output_location=temp_path)
 
-auth = None if Run.get_context().id.startswith("OfflineRun") else get_auth()
-print(auth)
-ws = Workspace.from_config(auth=auth)
+    experiment = Experiment(workspace = ws, name = EVAL_CONFIG.EXPERIMENT_NAME)
 
-download_model(ws=ws,
-               experiment_name=MODEL_CONFIG.EXPERIMENT_NAME,
-               run_id=MODEL_CONFIG.RUN_ID,
-               input_location=os.path.join(MODEL_CONFIG.INPUT_LOCATION, MODEL_CONFIG.NAME),
-               output_location=temp_path)
+    #Find/create a compute target.
+    try:
+        # Compute cluster exists. Just connect to it.
+        compute_target = ComputeTarget(workspace = ws, name = EVAL_CONFIG.CLUSTER_NAME)
+        print("Found existing compute target.")
+    except ComputeTargetException:
+        print("Creating a new compute target...")
+        compute_config = AmlCompute.provisioning_configuration(
+            vm_size = 'Standard_NC6',
+            max_nodes = 4
+        )
+        compute_target = ComputeTarget.create(workspace, cluster_name, compute_config)
+        compute_target.wait_for_completion(show_output = True, min_node_count = None, timeout_in_minutes = 20)
 
-experiment = Experiment(workspace = ws, name = EVAL_CONFIG.EXPERIMENT_NAME)
+    print("Compute target:", compute_target)
 
-#Find/create a compute target.
-try:
-    # Compute cluster exists. Just connect to it.
-    compute_target = ComputeTarget(workspace = ws, name = EVAL_CONFIG.CLUSTER_NAME)
-    print("Found existing compute target.")
-except ComputeTargetException:
-    print("Creating a new compute target...")
-    compute_config = AmlCompute.provisioning_configuration(
-        vm_size = 'Standard_NC6',
-        max_nodes = 4
+    dataset = ws.datasets[DATA_CONFIG.NAME]
+    print("dataset:", dataset)
+    print("TF supported versions:", TensorFlow.get_supported_versions())
+
+    #parameters used in the evaluation
+    script_params = {f"--MODEL_{k}": v for k, v in MODEL_CONFIG.items()}
+    script_params.update({f"--EVAL_{k}": v for k, v in EVAL_CONFIG.items()})
+    script_params.update({f"--DATA_{k}": v for k, v in DATA_CONFIG.items()})
+    script_params.update({f"--RESULT_{k}": v for k, v in RESULT_CONFIG.items()})
+    print("script_params:", script_params)
+
+    start = time.time()
+
+    tags= {}
+    # Specify pip packages here.
+    pip_packages = [
+        "azureml-dataprep[fuse,pandas]",
+        "glob2",
+        "opencv-python==4.1.2.30",
+        "matplotlib",
+        "tensorflow-addons==0.11.2",
+    ]
+
+    # Create the estimator.
+    estimator = TensorFlow(
+        source_directory=temp_path,
+        compute_target=compute_target,
+        entry_script="evaluate.py",
+        use_gpu=True,
+        framework_version="2.2",
+        inputs=[dataset.as_named_input("dataset").as_mount()],
+        pip_packages=pip_packages,
+        script_params=script_params
     )
-    compute_target = ComputeTarget.create(workspace, cluster_name, compute_config)
-    compute_target.wait_for_completion(show_output = True, min_node_count = None, timeout_in_minutes = 20)
 
-print("Compute target:", compute_target)
+    # Set compute target.
+    estimator.run_config.target = compute_target
 
-dataset = ws.datasets[DATA_CONFIG.NAME]
-print("dataset:", dataset)
-print("TF supported versions:", TensorFlow.get_supported_versions())
+    # Run the experiment.
+    run = experiment.submit(estimator, tags=tags)
 
-#parameters used in the evaluation
-script_params = {f"--MODEL_{k}": v for k, v in MODEL_CONFIG.items()}
-script_params.update({f"--EVAL_{k}": v for k, v in EVAL_CONFIG.items()})
-script_params.update({f"--DATA_{k}": v for k, v in DATA_CONFIG.items()})
-script_params.update({f"--RESULT_{k}": v for k, v in RESULT_CONFIG.items()})
-print("script_params:", script_params)
+    # Show run.
+    print("Run:", run)
 
-start = time.time()
+    #Check the logs of the current run until is complete
+    run.wait_for_completion(show_output=True)
 
-tags= {}
-# Specify pip packages here.
-pip_packages = [
-    "azureml-dataprep[fuse,pandas]",
-    "glob2",
-    "opencv-python==4.1.2.30",
-    "matplotlib",
-    "tensorflow-addons==0.11.2",
-]
+    #Print Completed when run is completed
+    print("Run status:", run.get_status())
 
-# Create the estimator.
-estimator = TensorFlow(
-    source_directory=temp_path,
-    compute_target=compute_target,
-    entry_script="evaluate.py",
-    use_gpu=True,
-    framework_version="2.2",
-    inputs=[dataset.as_named_input("dataset").as_mount()],
-    pip_packages=pip_packages,
-    script_params=script_params
-)
+    end = time.time()
+    print("Total time for evaluation experiment: {} sec".format(end-start))
 
-# Set compute target.
-estimator.run_config.target = compute_target
+    #Download the evaluation results of the model
+    GET_CSV_FROM_EXPERIMENT_PATH = '.'
+    run.download_file(RESULT_CONFIG.SAVE_PATH, GET_CSV_FROM_EXPERIMENT_PATH)
+    print("Downloaded the result.csv")
 
-# Run the experiment.
-run = experiment.submit(estimator, tags=tags)
+    result = pd.read_csv('result.csv')
+    print("Result:", result)
 
-# Show run.
-print("Run:", run)
-
-#Check the logs of the current run until is complete
-run.wait_for_completion(show_output=True)
-
-#Print Completed when run is completed
-print("Run status:", run.get_status())
-
-end = time.time()
-print("Total time for evaluation experiment: {} sec".format(end-start))
-
-#Download the evaluation results of the model
-GET_CSV_FROM_EXPERIMENT_PATH = '.'
-run.download_file(RESULT_CONFIG.SAVE_PATH, GET_CSV_FROM_EXPERIMENT_PATH)
-print("Downloaded the result.csv")
-
-result = pd.read_csv('result.csv')
-print("Result:", result)
-
-#Delete temp folder
-shutil.rmtree(temp_path)
+    #Delete temp folder
+    shutil.rmtree(temp_path)
