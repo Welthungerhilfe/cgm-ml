7a8e8677d372ff45d56cf9f3aeeff39644c95761
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2020-12-22 20:34:12 +0100

    Clean up

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -4,3 +4,5 @@ set -euox pipefail
 
 python eval_main.py --qa_config_module qa_config_42c4ef33
 python eval_main.py --qa_config_module qa_config_cb44f6db
+# python eval_main.py --qa_config_module qa_config_height
+# python eval_main.py --qa_config_module qa_config_weight
