07b44dfd16a0e305941b1eb5db7702cb9536ba66
prajurock <prajwalsingh651@gmail.com>
2021-01-16 14:44:19 +0530

    conufsion-matrix added

--- src/common/evaluation/QA/eval_depthmap_models/src/evaluate.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/evaluate.py
@@ -6,6 +6,7 @@ import random
 import time
 from importlib import import_module
 from pathlib import Path
+import json
 
 import glob2 as glob
 import numpy as np
@@ -15,8 +16,13 @@ from azureml.core import Experiment, Workspace
 from azureml.core.run import Run
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.python import keras
+from cgmzscore import Calculator
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
 
 import utils
+import seaborn as sns
 from constants import DATA_DIR_ONLINE_RUN, DEFAULT_CONFIG, REPO_DIR
 from utils import (AGE_IDX, COLUMN_NAME_AGE, COLUMN_NAME_GOODBAD,
                    COLUMN_NAME_SEX, GOODBAD_IDX, SEX_IDX,
@@ -24,7 +30,7 @@ from utils import (AGE_IDX, COLUMN_NAME_AGE, COLUMN_NAME_GOODBAD,
                    calculate_performance_goodbad, calculate_performance_sex,
                    download_dataset, draw_age_scatterplot,
                    draw_uncertainty_goodbad_plot, get_dataset_path,
-                   get_model_path)
+                   get_model_path,SEX_DICT)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -237,7 +243,7 @@ if __name__ == "__main__":
     print("Created dataset for training.")
 
     model_path = MODEL_BASE_DIR / get_model_path(MODEL_CONFIG)
-    prediction_list_one = get_prediction(model_path, dataset_evaluation)
+    prediction_list_one = get_prediction('/Users/prajwalsingh/cgm-ml/src/common/evaluation/QA/eval_depthmap_models/tmp_eval/best_model.h5', dataset_evaluation)
     print("Prediction made by model on the depthmaps...")
     print(prediction_list_one)
 
@@ -266,9 +272,34 @@ if __name__ == "__main__":
         df[COLUMN_NAME_GOODBAD] = [el[idx] for el in target_list]
 
     df_grouped = df.groupby(['qrcode', 'scantype']).mean()
-    print("Mean Avg Error: ", df_grouped)
+    #print("Mean Avg Error: ", df_grouped)
+    predicted_stunting=[]
+    actual_stunting=[]
+    not_processedData=[]
+    for index, row in df.iterrows():
+        sex='M'
+        if row['GT_sex']==SEX_DICT['female']:
+            sex='F'
+        elif row['GT_sex']==SEX_DICT['male']:
+            sex='M'
+        
+        if row['GT']> 45 and row['GT'] <=120 and row['predicted']> 45 and row['predicted']<=120 and row['GT_age']<=1856.0:
+            v = json.loads(Calculator().zScore_withclass(weight="0",muac="0",age_in_days=int(row['GT_age']),sex=sex,height=row['GT']))
+            actual_stunting.append(v['Class_HFA'])
+            k = json.loads(Calculator().zScore_withclass(weight="0",muac="0",age_in_days=int(row['GT_age']),sex=sex,height=row['predicted']))
+            predicted_stunting.append(k['Class_HFA'])
+        else:
+            not_processedData.append(row['qrcode'])
+
+    print(confusion_matrix(actual_stunting, predicted_stunting, labels=["Healthy", "Severly Stunted", "Moderately Stunted"]))
+    plt.figure(figsize=(10,7))
+    sns.heatmap(confusion_matrix(actual_stunting, predicted_stunting, labels=["Healthy", "Severly Stunted", "Moderately Stunted"]), annot=True)
+    plt.savefig("mygraph.png")
+
+
 
     df_grouped['error'] = df_grouped.apply(utils.avgerror, axis=1)
+    print(df_grouped)
 
     csv_file = f"{OUTPUT_CSV_PATH}/{RUN_ID}.csv"
     print(f"Calculate and save the results to {csv_file}")
