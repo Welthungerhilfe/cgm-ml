371df3697ab71a46164f3f47130ec868204ee03d
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-01-18 11:26:38 +0100

    Same for multiartifact

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifact-plaincnn-height/src/constants.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifact-plaincnn-height/src/constants.py
@@ -1,3 +1,15 @@
 from pathlib import Path
+from src.models.CNNDepthMap.CNNDepthMap-height.q3-depthmap-plaincnn-height.src.constants import (
+    PIP_PACKAGES)
 
 REPO_DIR = Path(__file__).parents[6].absolute()
+
+PIP_PACKAGES = [
+    "azureml-dataprep[fuse,pandas]",
+    "glob2",
+    "opencv-python==4.1.2.30",
+    "matplotlib",
+    "imgaug==0.4.0",
+    "tensorflow-addons==0.11.2",
+    "bunch==1.0.1",
+]
