dcd53099adb40628a46d9db68dcde3307c161cad
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-01-18 12:30:28 +0100

    Put draw_age_distribution() into a reusable function

--- src/data_utils/dataset_EDA/data_utilities.py
+++ src/data_utils/dataset_EDA/data_utilities.py
@@ -7,3 +7,10 @@ def convert_age_from_days_to_years(age_in_days):
 def extractqrcode(row):
     """Extract just the qrcode from them path"""
     return row['storage_path'].split('/')[1]
+
+
+def draw_age_distribution(scans):
+    age_ax = scans['Years'].value_counts().plot(kind='bar')
+    age_ax.set_xlabel('age')
+    age_ax.set_ylabel('no. of children')
+    print(scans['Years'].value_counts())
