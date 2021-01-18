8aaf3c7633cbe3a9005cf35285c6f2f74d3e19b5
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-01-18 12:16:08 +0100

    Put function extractqrcode() into utilities

--- src/data_utils/dataset_EDA/data_utilities.py
+++ src/data_utils/dataset_EDA/data_utilities.py
@@ -2,3 +2,11 @@ def convert_age_from_days_to_years(age_in_days):
     """Convert age in days into age in years"""
     age_in_years = age_in_days['age'] / 365
     return round(age_in_years)
+
+
+def extractqrcode(row):
+    """Extract just the qrcode from them path"""
+    complete_path = row['storage_path']
+    path = complete_path.split('/')
+    qrcode = path[1]
+    return qrcode
