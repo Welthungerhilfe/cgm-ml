93f490889effac6900216d28f5b88310756659a9
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-01-25 17:45:18 +0100

    Revert debug changes

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -2,7 +2,7 @@
 
 set -euox pipefail
 
-# python eval_main.py --qa_config_module qa_config_height_no_dropout
+python eval_main.py --qa_config_module qa_config_height_no_dropout
 python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_filter
 # python eval_main.py --qa_config_module qa_config_height
