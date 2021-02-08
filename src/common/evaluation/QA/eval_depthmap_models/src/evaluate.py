1475346672714e45d6c8e290a6def0dbc2597877
Prashant <pksingh@childgrowthmonitor.com>
2021-02-08 11:31:42 +0000

    Saving csv file

--- src/common/evaluation/QA/eval_depthmap_models/src/evaluate.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/evaluate.py
@@ -295,6 +295,8 @@ if __name__ == "__main__":
     print("df grouped")
     print(df_grouped)
     print("model_id:",RUN_ID)
+    
+    df_grouped.to_csv('sample.csv',index=False)
 
     csv_file = f"{OUTPUT_CSV_PATH}/{RUN_ID}.csv"
     print(f"Calculate and save the results to {csv_file}")
