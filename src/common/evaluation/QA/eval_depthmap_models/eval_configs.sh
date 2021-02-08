b65b39c4465f531a04b9f5c33f74e2bc89ee4475
prashant <Prashant905@gmail.com>
2021-02-08 15:59:29 +0530

    Update eval_configs.sh

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -2,7 +2,7 @@
 
 set -euox pipefail
 
- python eval_main.py --qa_config_module qa_config_height
+#python eval_main.py --qa_config_module qa_config_height
  python eval_main.py --qa_config_module qa_config_height_dropout
 #python eval_main.py --qa_config_module qa_config_filter
 # python eval_main.py --qa_config_module qa_config_height
