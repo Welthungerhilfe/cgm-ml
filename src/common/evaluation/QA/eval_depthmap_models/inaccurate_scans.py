e0555a490e317381d2ad49f70aa839e7480c352a
Prashant <pksingh@childgrowthmonitor.com>
2021-02-19 06:26:44 +0000

    adding function from utils to inaccurate scans filr

--- src/common/evaluation/QA/eval_depthmap_models/inaccurate_scans.py
+++ src/common/evaluation/QA/eval_depthmap_models/inaccurate_scans.py
@@ -5,28 +5,17 @@ from typing import List
 
 from glob2 import glob
 
-import utils
-
 OUTPUT_FILE_NAME = 'evaluated_models_result.csv'
 
 ACC = 2
 
-def combine_model_results(csv_file_list: List[str], output_path: str):
-    """Function to combine the models resultant csv files into a single file
+def merge_qrc(row):
+    scans = str(row['qrcode'])+'_'+str(row['scantype'])
+    return scans
 
-    Args:
-        csv_file_list: list containing absolute path of csv file
-        output_path: target folder path where to save result csv file
-    """
-    if len(csv_file_list) <= 0:
-        logging.warning("No csv files found in output directory to combine")
-        return
-    result_list = [pd.read_csv(results, index_col=0) for results in csv_file_list]
-    final_result = pd.concat(result_list, axis=0)
-    final_result = final_result.rename_axis("Model")
-    final_result = final_result.round(2)
-    result_csv = f"{output_path}/{OUTPUT_FILE_NAME}"
-    final_result.to_csv(result_csv, index=True)
+def filter_scans(dataframe: pd.DataFrame,accuracy: int):
+    error = dataframe[(dataframe['error'] >= accuracy) | (dataframe['error'] <= -accuracy)]
+    return error
     
 def inaccurate_scans(csv_file_list: List[str]):
     """Function to combine the models resultant csv files into a single file
@@ -42,8 +31,8 @@ def inaccurate_scans(csv_file_list: List[str]):
     for file in csv_file_list:
         result_list = pd.read_csv(file,index=False)
         grouped_result = result_list.groupby(['qrcode','scantype'],as_index=False).mean()
-        accuracy_df = utils.filter_scans(grouped_result,ACC)
-        accuracy_df['name'] = accuracy_df.apply(utils.merge_qrc,axis=1)
+        accuracy_df = filter_scans(grouped_result,ACC)
+        accuracy_df['name'] = accuracy_df.apply(merge_qrc,axis=1)
         csv_file = f"./outputs/{count}.csv"
         count+=1
         
