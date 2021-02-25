669a76a023f86ae03adb7dfe4228be754e064d23
Prashant <pksingh@childgrowthmonitor.com>
2021-02-25 06:16:10 +0000

    Print the csv files name

--- src/common/evaluation/QA/eval_depthmap_models/inaccurate_scans.py
+++ src/common/evaluation/QA/eval_depthmap_models/inaccurate_scans.py
@@ -7,9 +7,10 @@ from glob2 import glob
 from matplotlib_venn import venn2   
 from matplotlib import pyplot as plt
 
-OUTPUT_FILE_NAME = 'evaluated_models_result.csv'
+
 
 ACC = 2
+CSV_PATH = f"./outputs/**/*_scans.csv"
 
 def merge_qrc(row):
     scans = str(row['qrcode'])+'_'+str(row['scantype'])
@@ -29,9 +30,6 @@ def calculate_union(set1:set,set2:set):
 def calculate_intersection(set1:set,set2:set):
     intersection_set = set1.intersection(set2)
     return intersection_set
-
-
-
     
 
 def inaccurate_scans(file: List[str]):
@@ -48,20 +46,15 @@ def inaccurate_scans(file: List[str]):
     print("Length of the accuracy_df:",len(accuracy_df))
     frame_set = frame_to_set(accuracy_df)
     return frame_set
-#         csv_file = f'{file_path}/inaccurate_scan.csv'
-#         accuracy_df.to_csv(csv_file,index=True)
-   
-        
+
+if __name__ == "__main__": 
     
-
-
-if __name__ == "__main__":    
-    csv_path = f"./outputs/**/*_scans.csv"
-    csv_files = glob(csv_path)
+    csv_files = glob(CSV_PATH)
     if len(csv_files) <= 0:
-        logging.warning("No csv files found in output directory to combine")
+        logging.warning("No csv files found in output directory to combine.")
     scan_sets = []
     for file in csv_files:
+        print('files:',file)
         frame_set = inaccurate_scans(file)
         scan_sets.append(frame_set)
     
@@ -69,6 +62,6 @@ if __name__ == "__main__":
     Intersection_set = calculate_intersection(scan_sets[0],scan_sets[1])
     percentage  = (len(Intersection_set)/len(Union_set)) *100
     print("overlap_percentage:",percentage)
-    
+
 venn2(subsets = (len(scan_sets[0]),len(Intersection_set), len(scan_sets[1])), set_labels = ('dropout', 'no_dropout'))
 plt.savefig('venn.png')
