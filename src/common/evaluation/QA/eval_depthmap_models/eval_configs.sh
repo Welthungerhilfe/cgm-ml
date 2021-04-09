46e18746fb1fa8e095e216ed17de45a9f7d8f20d
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-04-09 10:27:53 +0200

    Evaluate 2 weight models: Run5 and Run6

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -2,16 +2,17 @@
 
 set -euox pipefail
 
-python eval_main.py --qa_config_module qa_config_mcnn
-python eval_main.py --qa_config_module qa_config_height_no_dropout
-python eval_main.py --qa_config_module qa_config_height_dropout
+# python eval_main.py --qa_config_module qa_config_mcnn
+# python eval_main.py --qa_config_module qa_config_height_no_dropout
+# python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_filter
 # python eval_main.py --qa_config_module qa_config_height
 python eval_main.py --qa_config_module qa_config_weight_no_dropout
-python eval_main.py --qa_config_module qa_config_weight_dropout
+python eval_main.py --qa_config_module qa_config_weight_no_dropout_run6
+# python eval_main.py --qa_config_module qa_config_weight_dropout
 
 # Combine results
-python combine_results.py --model_measurement height
+# python combine_results.py --model_measurement height
 
 # Find inaccurate results for different models
-python inaccurate_scans.py
+# python inaccurate_scans.py
