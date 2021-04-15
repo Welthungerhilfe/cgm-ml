de94d4abeea2789f2051fba396568bd718a0d347
prajurock <prajwalsingh651@gmail.com>
2021-04-15 18:41:56 +0530

    imports added

---
+++ experimental/HRNet-W48/inference.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import csv
+import os
+import shutil
+
+from PIL import Image
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision
+import cv2
+import numpy as np
