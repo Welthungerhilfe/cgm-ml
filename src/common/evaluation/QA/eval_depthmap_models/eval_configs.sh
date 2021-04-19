d4402d03f87191b2a7f8e7fa918549ab1577264f
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-04-19 09:35:21 +0200

    Eval height Run 9 and 10

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -2,8 +2,16 @@
 
 set -euox pipefail
 
-python eval_main.py --qa_config_module qa_config_mcnn
-python eval_main.py --qa_config_module qa_config_height_no_dropout
+python eval_main.py --qa_config_module  qa_config_height_Run10
+python eval_main.py --qa_config_module  qa_config_height_Run9
+#python eval_main.py --qa_config_module qa_config_mcnn
+#python eval_main.py --qa_config_module qa_config_height_no_dropout
+
+
+
+
+
+
 # python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_filter
 # python eval_main.py --qa_config_module qa_config_height
