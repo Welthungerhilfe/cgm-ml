231d80a46e54a620d075d08d000c976de2b88802
prajurock <prajwalsingh651@gmail.com>
2021-04-26 11:07:51 +0530

    experiment updated

--- experimental/HRNet-W48/inference.py
+++ experimental/HRNet-W48/inference.py
@@ -6,6 +6,7 @@ import argparse
 import csv
 import os
 import shutil
+import time
 
 from PIL import Image
 import torch
@@ -22,11 +23,11 @@ import numpy as np
 import sys
 sys.path.append("experimental/HRNet-W48/lib")  # noqa:403
 
-#import models
+import models
 from config import cfg
 from config import update_config
-#from core.inference import get_final_preds
-#from utils.transforms import get_affine_transform
+from core.inference import get_final_preds
+from utils.transforms import get_affine_transform
 
 CTX = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -66,12 +67,55 @@ COCO_INSTANCE_CATEGORY_NAMES = [
 ]
 
 
+def box_to_center_scale(box, model_image_width, model_image_height):
+    """convert a box to center,scale information required for pose transformation
+    Parameters
+    ----------
+    box : list of tuple
+        list of length 2 with two tuples of floats representing
+        bottom left and top right corner of a box
+    model_image_width : int
+    model_image_height : int
+
+    Returns
+    -------
+    (numpy array, numpy array)
+        Two numpy arrays, coordinates for the center of the box and the scale of the box
+    """
+    center = np.zeros((2), dtype=np.float32)
+
+    bottom_left_corner = box[0]
+    top_right_corner = box[1]
+    box_width = top_right_corner[0] - bottom_left_corner[0]
+    box_height = top_right_corner[1] - bottom_left_corner[1]
+    bottom_left_x = bottom_left_corner[0]
+    bottom_left_y = bottom_left_corner[1]
+    center[0] = bottom_left_x + box_width * 0.5
+    center[1] = bottom_left_y + box_height * 0.5
+
+    aspect_ratio = model_image_width * 1.0 / model_image_height
+    pixel_std = 200
+
+    if box_width > aspect_ratio * box_height:
+        box_height = box_width * 1.0 / aspect_ratio
+    elif box_width < aspect_ratio * box_height:
+        box_width = box_height * aspect_ratio
+    scale = np.array(
+        [box_width * 1.0 / pixel_std, box_height * 1.0 / pixel_std],
+        dtype=np.float32)
+    if center[0] != -1:
+        scale = scale * 1.25
+
+    return center, scale
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Train keypoints network')
     # general
     parser.add_argument('--cfg', type=str,
                         default='/Users/prajwalsingh/cgm-ml/experimental/HRNet-W48/inference-config.yaml')
-    parser.add_argument('--videoFile', type=str, default='/Users/prajwalsingh/cgm-ml/data/anon-rgb-classification/test/scan/qrcode/1585352016-s51bhrzmtt/measure/1592712865086/rgb/rgb_1585352016-s51bhrzmtt_1592712865086_100_520010.538748759.jpg')
+    parser.add_argument('--videoFile', type=str,
+                        default='/Users/prajwalsingh/cgm-ml/data/anon-rgb-classification/train/laying/rgb_1598374858-tpsqgmxe87_1598374858319_201_260565.08058466003.jpg')
     parser.add_argument('--outputDir', type=str, default='/output/')
     parser.add_argument('--inferenceFps', type=int, default=10)
     parser.add_argument('--writeBoxFrames', action='store_true')
@@ -99,6 +143,59 @@ def prepare_output_dirs(prefix='/Users/prajwalsingh/cgm-ml/experimental/HRNet-W4
     return pose_dir
 
 
+def get_pose_estimation_prediction(pose_model, image, centers, scales, transform):
+    rotation = 0
+
+    # pose estimation transformation
+    model_inputs = []
+    for center, scale in zip(centers, scales):
+        trans = get_affine_transform(center, scale, rotation, cfg.MODEL.IMAGE_SIZE)
+        # Crop smaller image of people
+        model_input = cv2.warpAffine(
+            image,
+            trans,
+            (int(cfg.MODEL.IMAGE_SIZE[0]), int(cfg.MODEL.IMAGE_SIZE[1])),
+            flags=cv2.INTER_LINEAR)
+
+        # hwc -> 1chw
+        model_input = transform(model_input)  # .unsqueeze(0)
+        model_inputs.append(model_input)
+
+    # n * 1chw -> nchw
+    model_inputs = torch.stack(model_inputs)
+
+    # compute output heatmap
+    output = pose_model(model_inputs.to(CTX))
+    coords, _ = get_final_preds(
+        cfg,
+        output.cpu().detach().numpy(),
+        np.asarray(centers),
+        np.asarray(scales))
+
+    return coords
+
+
+def get_person_detection_boxes(model, img, threshold=0.5):
+    pil_image = Image.fromarray(img)  # Load the image
+    transform = transforms.Compose([transforms.ToTensor()])  # Defing PyTorch Transform
+    transformed_img = transform(pil_image)  # Apply the transform to the image
+    pred = model([transformed_img.to(CTX)])  # Pass the image to the model
+    # Use the first detected person
+    pred_classes = [COCO_INSTANCE_CATEGORY_NAMES[i]
+                    for i in list(pred[0]['labels'].cpu().numpy())]  # Get the Prediction Score
+    pred_boxes = [[(i[0], i[1]), (i[2], i[3])]
+                  for i in list(pred[0]['boxes'].cpu().detach().numpy())]  # Bounding boxes
+    pred_scores = list(pred[0]['scores'].cpu().detach().numpy())
+
+    person_boxes = []
+    # Select box has score larger than threshold and is person
+    for pred_class, pred_box, pred_score in zip(pred_classes, pred_boxes, pred_scores):
+        if (pred_score > threshold) and (pred_class == 'person'):
+            person_boxes.append(pred_box)
+
+    return person_boxes
+
+
 def main():
     # transformation
     pose_transform = transforms.Compose([
@@ -122,9 +219,71 @@ def main():
     box_model.to(CTX)
     box_model.eval()
 
-    image_bgr = cv2.imread(
-        args.videoFile, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+    image = cv2.imread(args.videoFile)
+    #image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+    image_per = image.copy()
+    image_pose = image.copy()
+
+    image_debug = image.copy()
+
+    now = time.time()
+    pred_boxes = get_person_detection_boxes(box_model, image_per, threshold=0.9)
+    then = time.time()
+    count = 0
+
+    #cv2.imshow('image', pred_boxes)
+    print("Find person bbox in: {} sec".format(then - now))
+
+    for box in pred_boxes:
+        cv2.rectangle(image_debug, box[0], box[1], color=(0, 255, 0),
+                      thickness=3)
+
+    if not pred_boxes:
+        print("No child predicted")
+    # else:
+        #cv2.imwrite('img_file', image_debug)
+    print('models.' + cfg.MODEL.NAME + '.get_pose_net')
+    pose_model = eval('models.' + cfg.MODEL.NAME + '.get_pose_net')(
+        cfg, is_train=True
     )
+
+    centers = []
+    scales = []
+    for box in pred_boxes:
+        center, scale = box_to_center_scale(box, cfg.MODEL.IMAGE_SIZE[0], cfg.MODEL.IMAGE_SIZE[1])
+        centers.append(center)
+        scales.append(scale)
+
+    now = time.time()
+    pose_preds = get_pose_estimation_prediction(pose_model, image_pose, centers, scales, transform=pose_transform)
+    print(pose_preds)
+    then = time.time()
+    print("Find person pose in: {} sec".format(then - now))
+
+    new_csv_row = []
+    for coords in pose_preds:
+        # Draw each point on image
+        for coord in coords:
+            x_coord, y_coord = int(coord[0]), int(coord[1])
+            cv2.circle(image_debug, (x_coord, y_coord), 4, (255, 0, 0), 2)
+            new_csv_row.extend([x_coord, y_coord])
+
+    total_then = time.time()
+
+    text = "1s"
+    # cv2.putText(image_debug, text, (100, 50), cv2.FONT_HERSHEY_SIMPLEX,
+    #                   1, (0, 0, 255), 2, cv2.LINE_AA)
+
+    #cv2.imshow("pos", image_debug)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #   break
+
+    csv_output_rows.append(new_csv_row)
+    img_file = os.path.join(pose_dir, 'pose_{:08d}.jpg'.format(count))
+    print(img_file)
+    cv2.imwrite(img_file, image_debug)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
