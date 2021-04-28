fb0d0a3aeadb60bba2f8352f66d8a42a22140d2c
prajurock <prajwalsingh651@gmail.com>
2021-04-28 20:22:56 +0530

    qa_conifg updated

--- src/common/evaluation/QA/eval_depthmap_models/src/qa_config_height_deep_ensemble.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/qa_config_height_deep_ensemble.py
@@ -1,9 +1,13 @@
+import os
+
 from bunch import Bunch
+
+CONFIG_NAME = os.path.splitext(os.path.basename(__file__))[0]
 
 # Details of model used for evaluation
 MODEL_CONFIG = Bunch(dict(
     EXPERIMENT_NAME='q1-ensemble-warmup',
-    RUN_IDS='all',
+    RUN_ID='all',
 
     INPUT_LOCATION='outputs',
     NAME='best_model.ckpt',
@@ -19,7 +23,7 @@ EVAL_CONFIG = Bunch(dict(
     CLUSTER_NAME="gpu-cluster",
 
     # Used for Debug the QA pipeline
-    DEBUG_RUN=False,
+    DEBUG_RUN=True,
 
     # Will run eval on specified # of scan instead of full dataset
     DEBUG_NUMBER_OF_SCAN=5,
@@ -37,7 +41,7 @@ DATA_CONFIG = Bunch(dict(
     BATCH_SIZE=512,  # Batch size for evaluation
     NORMALIZATION_VALUE=7.5,
 
-    TARGET_INDEXES=[0],  # 0 is height, 1 is weight.
+    TARGET_INDEXES=[0, 5],  # 0 is height, 1 is weight.
     CODES=['100', '101', '102', '200', '201', '202']
 ))
 
@@ -47,8 +51,14 @@ RESULT_CONFIG = Bunch(dict(
     # Error margin on various ranges
     #EVALUATION_ACCURACIES = [.2, .4, .8, 1.2, 2., 2.5, 3., 4., 5., 6.]
     ACCURACIES=[.2, .4, .6, 1, 1.2, 2., 2.5, 3., 4., 5., 6.],  # 0.2cm, 0.4cm, 0.6cm, 1cm, ...
+    ACCURACY_MAIN_THRESH=1.0,
     COLUMNS=['qrcode', 'artifact', 'scantype', 'GT', 'predicted'],
 
+    USE_UNCERTAINTY=True,  # Flag to enable model uncertainty calculation
+    NUM_DROPOUT_PREDICTIONS=16,
+    DROPOUT_STRENGTH=1,  # 1.0 means like original model
+    UNCERTAINTY_THRESHOLD_IN_CM=4.,
+    TARGET_INDEXES=[0],
     # path of csv file in the experiment which final result is stored
-    SAVE_PATH='./outputs/result.csv',
+    SAVE_PATH=f'./outputs/{CONFIG_NAME}',
 ))
