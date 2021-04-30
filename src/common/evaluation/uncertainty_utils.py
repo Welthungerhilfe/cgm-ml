7f7ce83869d57807721bb720092feb7ee67a4a13
prajurock <prajwalsingh651@gmail.com>
2021-04-30 11:42:15 +0530

    get Prediction uncertainitis for list added

--- src/common/evaluation/uncertainty_utils.py
+++ src/common/evaluation/uncertainty_utils.py
@@ -59,3 +59,32 @@ def predict_uncertainty(X: np.array, model: tf.keras.Model, num_dropout_predicti
     predictions = model(one_batch, training=True)
     std = tf.math.reduce_std(predictions)
     return std
+
+
+def get_prediction_uncertainty(model_paths: list, dataset_evaluation: tf.data.Dataset) -> np.array:
+    """Predict standard deviation of multiple predictions with different dropouts
+    Args:
+        model_path: Path of the trained model
+        dataset_evaluation: dataset in which the evaluation need to performed
+    Returns:
+        predictions, array shape (N_SAMPLES, )
+    """
+
+    dataset = dataset_evaluation.batch(1)
+
+    print("starting predicting uncertainty")
+
+    # Go through all models and compute STD of predictions.
+    start = time.time()
+    std_list = []
+    for model_path in model_paths:
+        print(f"loading model from {model_path}")
+        model = load_model(model_path, compile=False)
+        std_list += [[model.predict(X)[0] for X, y in dataset.as_numpy_iterator()]]
+    std_list = np.array(std_list)
+    std_list = np.std(std_list, axis=0)
+    std_list = std_list.reshape((-1))
+    end = time.time()
+    print(f"Total time for uncertainty prediction experiment: {end - start:.3} sec")
+
+    return np.array(std_list)
