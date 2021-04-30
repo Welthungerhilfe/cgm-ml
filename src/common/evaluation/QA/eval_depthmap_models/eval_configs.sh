c6e7c7642f4397b3fa25b41f4f9e2cf59833331f
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-04-30 19:48:08 +0200

    Calculate on clean

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -6,7 +6,7 @@ set -euox pipefail
 python eval_main.py --qa_config_module qa_config_height
 # python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_height_filter
-# python eval_main.py --qa_config_module qa_config_height_clean
+python eval_main.py --qa_config_module qa_config_height_clean
 # python eval_main.py --qa_config_module qa_config_weight
 # python eval_main.py --qa_config_module qa_config_weight_dropout
 
