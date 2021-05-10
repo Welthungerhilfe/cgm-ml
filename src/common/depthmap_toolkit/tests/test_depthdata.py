deef226939a47ce32eab5f18ab8a8ac29f0cfb4e
shanky0009 <shashank190897@gmail.com>
2021-05-10 16:58:21 +0530

    added unit test to check depth file loading

---
+++ src/common/depthmap_toolkit/tests/test_depthdata.py
@@ -0,0 +1,22 @@
+import pathlib
+import sys
+
+sys.path.append('./src/common/depthmap_toolkit')
+import utils, depthmap
+
+
+CWD = pathlib.Path.cwd()
+WIDTH = 240
+HEIGHT = 180
+
+def test_extract_depthmap():
+    # Setup
+    extracted_depth_file = CWD.joinpath('tests', 'static_files', 'data')
+
+    # Exercise
+    data, width, height, depthScale, max_confidence, matrix = utils.parse_data(extracted_depth_file)
+
+    # Verify
+    assert (width, height) == (WIDTH, HEIGHT)
+
+    # Cleanup - none required
