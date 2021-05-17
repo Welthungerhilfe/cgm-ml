62f0cd10396de7c9b1fa6dfa447948dbc1545d76
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-05-17 13:40:40 +0200

    Implement transform

--- src/data_utils/dataset_generation_azurepipeline/dataprep_src/prepare.py
+++ src/data_utils/dataset_generation_azurepipeline/dataprep_src/prepare.py
@@ -7,9 +7,23 @@ from azureml.core import Experiment, Workspace
 from azureml.core.run import Run
 import pandas as pd
 
+from mlpipeline_utils import process_artifact_tuple
 
 REPO_DIR = Path(__file__).parents[4].absolute()
-print(f"REPO_DIR: {REPO_DIR}")
+
+
+def download_dataset(workspace: Workspace, dataset_name: str, dataset_path: str):
+    logging.info("Accessing dataset...")
+    if os.path.exists(dataset_path):
+        return
+    dataset = workspace.datasets[dataset_name]
+    logging.info("Downloading dataset %s", dataset_name)
+    dataset.download(target_path=dataset_path, overwrite=False)
+    logging.info("Finished downloading %s", dataset_name)
+
+def get_dataset_path(data_dir: Path, dataset_name: str) -> str:
+    return str(data_dir / dataset_name)
+
 
 
 def print_blob_files(path):
@@ -20,32 +34,41 @@ def print_blob_files(path):
     print(files[:3])
 
 
-print("Arguments: ")
-print(sys.argv)
+if __name__ == '__main__':
+    print("Arguments: ")
+    print(sys.argv)
 
-# Setup
-run = Run.get_context()
-if run.id.startswith("OfflineRun"):
-    workspace = Workspace.from_config()
-    experiment = Experiment(workspace, "training-junkyard")
-else:
-    experiment = run.experiment
-    workspace = experiment.workspace
-
-
-# SQL dataset
-if run.id.startswith("OfflineRun"):
-    df = pd.read_csv(REPO_DIR / 'data' / 'sql_query_result.csv')
-else:
-    tabular_dataset = run.input_datasets['input1']
-    df = tabular_dataset.to_pandas_dataframe()
-print("CGM Dataframe:")
-print(df.head())
+    # Setup
+    run = Run.get_context()
+    if run.id.startswith("OfflineRun"):
+        workspace = Workspace.from_config(Path(__file__).parents[1] / 'config.py')
+        experiment = Experiment(workspace, "training-junkyard")
+    else:
+        experiment = run.experiment
+        workspace = experiment.workspace
 
 
-# Blob dataset
-if run.id.startswith("OfflineRun"):
-    pass
-else:
-    blob_dataset_path = run.input_datasets['input2']
-    print_blob_files(Path(blob_dataset_path))
+    # SQL dataset
+    if run.id.startswith("OfflineRun"):
+        df = pd.read_csv(REPO_DIR / 'data' / 'sql_query_result.csv')
+    else:
+        tabular_dataset = run.input_datasets['input1']
+        df = tabular_dataset.to_pandas_dataframe()
+    print("CGM Dataframe:")
+    print(df.head())
+
+
+    # Blob dataset
+    if run.id.startswith("OfflineRun"):
+        dataset_name = 'cgm-result-dataset'
+        blob_dataset_path = get_dataset_path(REPO_DIR / 'data', dataset_name)
+        download_dataset(workspace, dataset_name, blob_dataset_path)
+    else:
+        blob_dataset_path = run.input_datasets['input2']
+        print_blob_files(Path(blob_dataset_path))
+
+
+    # Transform
+    for query_result in df.itertuples(index=False):
+        res = process_artifact_tuple(query_result)
+        print(f"res{str(res)}")
