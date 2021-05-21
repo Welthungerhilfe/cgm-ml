33b897f25e15301e5c2ddc3ae42383c813084b61
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-05-21 09:22:38 +0200

    De-duplicate identity matrix

--- src/common/depthmap_toolkit/utils.py
+++ src/common/depthmap_toolkit/utils.py
@@ -7,6 +7,11 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s - %(pathname)s: line %(lineno)d')
 
+IDENTITY_MATRIX_4D = [1., 0., 0., 0.,
+                      0., 1., 0., 0.,
+                      0., 0., 1., 0.,
+                      0., 0., 0., 1.]
+
 
 def cross(a: list, b: list) -> list:
     """Cross product of two vectors"""
@@ -32,10 +37,7 @@ def norm(v: list) -> list:
 def matrix_calculate(position: list, rotation: list) -> list:
     """Calculate a matrix image->world from device position and rotation"""
 
-    output = [1., 0., 0., 0.,
-              0., 1., 0., 0.,
-              0., 0., 1., 0.,
-              0., 0., 0., 1.]
+    output = IDENTITY_MATRIX_4D
 
     sqw = rotation[3] * rotation[3]
     sqx = rotation[0] * rotation[0]
@@ -263,10 +265,7 @@ def parse_data(filename: str):
             rotation = (float(header[3]), float(header[4]), float(header[5]), float(header[6]))
             matrix = matrix_calculate(position, rotation)
         else:
-            matrix = [1., 0., 0., 0.,
-                      0., 1., 0., 0.,
-                      0., 0., 1., 0.,
-                      0., 0., 0., 1.]
+            matrix = IDENTITY_MATRIX_4D
         data = f.read()
         f.close()
 
@@ -276,7 +275,7 @@ def parse_data(filename: str):
 def parse_depth(tx: int, ty: int, width: int, height: int, data: bytes, depth_scale: float) -> float:
     """Get depth of the point in meters"""
     if tx < 1 or ty < 1 or tx >= width or ty >= height:
-        return 0
+        return 0.
     depth = data[(int(ty) * width + int(tx)) * 3 + 0] << 8
     depth += data[(int(ty) * width + int(tx)) * 3 + 1]
     depth *= depth_scale
