41841152ed64da4de948150205c3a85cac726a87
jasminziegler <ziegler-jasmin@outlook.de>
2021-06-01 11:25:46 +0200

    added test for copy_dir in evaluate.py

--- src/common/evaluation/QA/eval_depthmap_models/src/test_evaluate.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/test_evaluate.py
@@ -1,44 +1,22 @@
 import sys
 from pathlib import Path
 
+from evaluate import copy_dir 
+
 here = Path(__file__).resolve()
 
 sys.path.append(str(here.parents[1])) # noqa
 
-#from evaluation.QA.eval_depthmap_models.evaluate import copy_dir 
-#from evaluate import copy_dir 
 REPO_DIR = str(here.parents[6].absolute())
 
-print("Before")
-print(REPO_DIR)
 
-""" def test_copy_dir():
-    print("Inside")
-    common_dir_path = REPO_DIR / "src/common"
-    temp_common_dir = Path(__file__).parent / "temp_common"
+def test_copy_dir():
+    common_dir_path = Path(REPO_DIR + "/src/common")
+    print("common_dir_path: ", common_dir_path)
+    temp_common_dir = Path(here.parent / "temp_common")
+    print("temp_common_dir: ", temp_common_dir)
     copy_dir(src=common_dir_path, tgt=temp_common_dir, glob_pattern='*/*.py', should_touch_init=True)
     if temp_common_dir.exists():
-        print("Temp folder exists")
+        print("copying successful, temp_common folder was created.")
 
-test_copy_dir() """
-
-""" def copy_dir(src: Path, tgt: Path, glob_pattern: str, should_touch_init: bool = False):
-    logging.info("Creating temp folder")
-    if tgt.exists():
-        shutil.rmtree(tgt)
-    tgt.mkdir(parents=True, exist_ok=True)
-    if should_touch_init:
-        (tgt / '__init__.py').touch(exist_ok=False)
-
-    paths_to_copy = list(src.glob(glob_pattern))
-    logging.info(f"Copying to {tgt} the following files: {str(paths_to_copy)}")
-    for p in paths_to_copy:
-        destpath = tgt / p.relative_to(src)
-        destpath.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy(p, destpath) """
-
-# so wird sie aufgerufen
-    # Copy common into the temp folder
-    #common_dir_path = REPO_DIR / "src/common"
-    #temp_common_dir = Path(__file__).parent / "temp_common"
-    #copy_dir(src=common_dir_path, tgt=temp_common_dir, glob_pattern='*/*.py', should_touch_init=True)
+test_copy_dir()
