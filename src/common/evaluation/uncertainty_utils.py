cbfb04ae3ad93c09399533c4b3ef210f6fc65b7f
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-06-02 10:50:53 +0200

    Remove unused MC dropout uncertainty code

--- src/common/evaluation/uncertainty_utils.py
+++ src/common/evaluation/uncertainty_utils.py
@@ -21,46 +21,6 @@ def change_dropout_strength(model: tf.keras.Model, dropout_strength: float) -> t
     return new_model
 
 
-def get_prediction_uncertainty(model_path: str, dataset_evaluation: tf.data.Dataset, dropout_strength: float, num_dropout_predictions: int) -> np.array:
-    """Predict standard deviation of multiple predictions with different dropouts
-
-    Args:
-        model_path: Path of the trained model
-        dataset_evaluation: dataset in which the evaluation need to performed
-
-    Returns:
-        predictions, array shape (N_SAMPLES, )
-    """
-    logging.info("loading model from %s", model_path)
-    model = load_model(model_path, compile=False)
-    model = change_dropout_strength(model, dropout_strength)
-
-    dataset = dataset_evaluation.batch(1)
-
-    logging.info("starting predicting uncertainty")
-    start = time.time()
-    std_list = [predict_uncertainty(X, model, num_dropout_predictions) for X, y in dataset.as_numpy_iterator()]
-    end = time.time()
-    logging.info("Total time for uncertainty prediction experiment: %.2f sec", end - start)
-
-    return np.array(std_list)
-
-
-def predict_uncertainty(X: np.array, model: tf.keras.Model, num_dropout_predictions: int) -> float:
-    """Predict standard deviation of multiple predictions with different dropouts
-    Args:
-        X: Sample image with shape (1, h, w, 1)
-        model: keras model
-
-    Returns:
-        The standard deviation of multiple predictions
-    """
-    one_batch = np.repeat(X, num_dropout_predictions, axis=0)
-    predictions = model(one_batch, training=True)
-    std = tf.math.reduce_std(predictions)
-    return std
-
-
 def get_prediction_uncertainty_deepensemble(model_paths: list, dataset_evaluation: tf.data.Dataset) -> np.array:
     """Predict standard deviation of multiple predictions with different dropouts
     Args:
