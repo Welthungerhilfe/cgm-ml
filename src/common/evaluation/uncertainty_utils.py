35b2125a02b3f8289c5e56bdee3fb2c659d01b79
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-06-03 14:44:27 +0200

    Extract _load_and_predict()

--- src/common/evaluation/uncertainty_utils.py
+++ src/common/evaluation/uncertainty_utils.py
@@ -7,23 +7,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-BATCH_SIZE = 8
-
-
-def _predict(model: tf.Module, dataset: tf.data.Dataset) -> np.array:
-    predictions_batches = []
-    for X, _y in dataset.as_numpy_iterator():
-        prediction_batch = model.predict(X)  # shape (BATCH_SIZE, 1)
-        predictions_batches.append(prediction_batch)
-    predictions = np.concatenate(predictions_batches)
-    return predictions
-
-
-def _calculate_std(predictions_per_model: List[np.array]) -> np.array:
-    predictions_per_model_ = np.array(predictions_per_model)
-    std = np.std(predictions_per_model_, axis=0)
-    return std
-
 
 def get_prediction_uncertainty_deepensemble(model_paths: list,
                                             dataset_evaluation: tf.data.Dataset,
@@ -40,14 +23,31 @@ def get_prediction_uncertainty_deepensemble(model_paths: list,
 
     # Go through all models and compute standard deviation of predictions
     start = time.time()
-    predictions_list = []
-    for model_path in model_paths:
-        logging.info("Loading model from %s", model_path)
-        model = load_model(model_path, compile=False)
-        logging.info("Predicting with model %s", model_path)
-        predictions_list.append(_predict(model, dataset))
+    predictions_list = [_load_and_predict(model_path, dataset) for model_path in model_paths]
     end = time.time()
     logging.info("Total time for uncertainty prediction experiment: %f:.3 sec", end - start)
 
-    std = _calculate_std()
+    std = _calculate_std(predictions_list)
+    return std
+
+
+def _load_and_predict(model_path, dataset):
+    logging.info("Loading model from %s", model_path)
+    model = load_model(model_path, compile=False)
+    logging.info("Predicting with model %s", model_path)
+    return _predict(model, dataset)
+
+
+def _predict(model: tf.Module, dataset: tf.data.Dataset) -> np.array:
+    predictions_batches = []
+    for X, _y in dataset.as_numpy_iterator():
+        prediction_batch = model.predict(X)  # shape (BATCH_SIZE, 1)
+        predictions_batches.append(prediction_batch)
+    predictions = np.concatenate(predictions_batches)
+    return predictions
+
+
+def _calculate_std(predictions_per_model: List[np.array]) -> np.array:
+    predictions_per_model_ = np.array(predictions_per_model)
+    std = np.std(predictions_per_model_, axis=0)
     return std
