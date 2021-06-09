419e4c3cff049d05ff2bf866cea07f71fc6da2a2
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-06-09 20:35:33 +0200

    Add MANIFEST.in, add setup.cfg, enhance setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cgmml",
+    name="cgm-ml-common",
     version="3.0.0-alpha",
     author="Markus Hinsche",
     author_email="mhinsche@childgrowthmonitor.com",
-    description="cgm-ml",
+    url="https://github.com/Welthungerhilfe/cgm-ml",
+
+    description="ChildGrowthMonitor's ML Common code",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Welthungerhilfe/cgm-ml",
+
     classifiers=[
         'Intended Audience :: Healthcare Industry',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GPL-3.0',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
+
     package_dir={"": "src"},
     packages=find_packages(
         include="common/model_utils",
         exclude=['docs', 'common/model_utils/tests'],
         ),
+
     python_requires=">=3.6",
+    setup_requires=['pytest-runner', 'flake8'],
+    tests_require=['pytest'],
 )
