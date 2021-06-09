2613c780de0dd9a5bdd2ae39a9294989979e71b2
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-06-09 21:47:52 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cgm-ml-common",
-    version="3.0.0-alpha",
+    version="3.0.0-alpha2",
     author="Markus Hinsche",
     author_email="mhinsche@childgrowthmonitor.com",
     url="https://github.com/Welthungerhilfe/cgm-ml",
