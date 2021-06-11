4e3a50e1596ccff568308ce652911af62629bb9f
prajurock <prajwalsingh651@gmail.com>
2021-06-11 12:33:58 +0530

    deployment created

---
+++ src/common/endpoints/deploy.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+from pathlib import Path
+from config import CONFIG
+
+from constants import REPO_DIR
+import numpy as np
+from azureml.core import Environment, Model, Workspace
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.model import InferenceConfig, Model
+from azureml.core.webservice import AciWebservice, LocalWebservice, Webservice
+
+ws = Workspace.from_config()
+model = Model(ws, name=CONFIG.MODEL_NAME)
+
+cgm_env = Environment.from_conda_specification(
+    name='project_environment', file_path=REPO_DIR / "environment_train.yml")
+cgm_env.docker.enabled = True
+cgm_env.docker.base_image = 'mcr.microsoft.com/azureml/openmpi3.1.2-cuda10.1-cudnn7-ubuntu18.04'
+inference_config_aci = InferenceConfig(
+    environment=cgm_env,
+    entry_script=str(REPO_DIR / "src/common/endpoints/entry_deep.py"),
+)
+
+
+deployment_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=4)
+
+#deployment_config = LocalWebservice.deploy_configuration(port=6789)
+service = Model.deploy(ws, CONFIG.ENDPOINT_NAME, [model], inference_config_aci, deployment_config, overwrite=True,)
+service.wait_for_deployment(show_output=True)
+print(service.swagger_uri)
