7e06feada00483f4ed382c1f9dc472d274c198d6
prajurock <prajwalsingh651@gmail.com>
2021-06-11 12:34:09 +0530

    modal registered

---
+++ src/common/endpoints/register.py
@@ -0,0 +1,10 @@
+from azureml.core import Workspace, Experiment, Run
+from azureml.core import ScriptRunConfig
+from config import CONFIG
+
+ws = Workspace.from_config()
+exp = Experiment(workspace=ws, name=CONFIG.EXPERIMENT_NAME)
+run = Run(exp, CONFIG.RUN_ID)
+model = run.register_model(model_name=CONFIG.MODEL_NAME,
+                           model_path='outputs')
+print('Model register successfully')
