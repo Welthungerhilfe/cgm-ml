6b8094a1a402dfdc7af4c0ddb6ce5e5cc39af836
prajurock <prajwalsingh651@gmail.com>
2021-06-11 20:51:11 +0530

    path corrected

--- src/common/endpoints/entry_script_aci.py
+++ src/common/endpoints/entry_script_aci.py
@@ -5,9 +5,8 @@ from tensorflow.keras.models import load_model
 
 
 def init():
-    global model  # pylint: disable=global-statement
-    model = load_model(
-        str('/var/azureml-app/azureml-models' / 'Deepensemble' / '5' / 'outputs/best_model.ckpt/'), compile=False)
+    global model
+    model = load_model('/var/azureml-app/azureml-models/Deepensemble/5/outputs/best_model.ckpt/', compile=False)
 
 
 def run(data):
