29d3d07440cc8077ae4b492841c47762dc6894b5
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-06-16 21:52:00 +0200

    Make flake8 happy

--- src/common/data_utilities/tests/test_mlpipeline_utils.py
+++ src/common/data_utilities/tests/test_mlpipeline_utils.py
@@ -5,7 +5,7 @@ from tempfile import TemporaryDirectory
 
 sys.path.append('./src/common/data_utilities')
 from mlpipeline_utils import (  # noqa: E402
-    load_depth, get_depthmaps, ArtifactProcessor, IMAGE_TARGET_HEIGHT, IMAGE_TARGET_WIDTH)
+    load_depth, get_depthmaps, ArtifactProcessor)
 
 DATA_UTILITIES_DIR = Path(__file__).parents[1].absolute()
 ARTIFACT_ZIP_PATH = 'be1faf54-69c7-11eb-984b-a3ffd42e7b5a/depth/bd67cd9e-69c7-11eb-984b-77ac9d2b4986'
