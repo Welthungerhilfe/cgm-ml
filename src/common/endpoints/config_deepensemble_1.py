6c57fb250f13a8aa1050992bf412640424696dde
Prajwal kumar singh <prajwalsingh651@gmail.com>
2021-06-16 15:38:56 +0530

    Update config_deepensemble_1.py

--- src/common/endpoints/config_deepensemble_1.py
+++ src/common/endpoints/config_deepensemble_1.py
@@ -4,7 +4,7 @@ from constants import REPO_DIR
 CONFIG = Bunch(dict(
     MODEL_NAME="2021q1-depthmap-ensemble-height-95k",
     VERSION='1',
-    ENDPOINT_NAME='aci-deep-ensemble-height-s2',
+    ENDPOINT_NAME='aci-deep-ensemble-height-v1-1',
     EXPERIMENT_NAME='2021q1-depthmap-ensemble-height-95k',
     RUN_ID='2021q1-depthmap-ensemble-height-95k_1622230334_67c64a77',
     LOCALTEST=False,
