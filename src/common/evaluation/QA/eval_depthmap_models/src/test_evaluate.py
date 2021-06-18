d3f5760f927bae8bb460b7a4661b0ca1346fceb9
jasminziegler <ziegler-jasmin@outlook.de>
2021-06-18 13:09:31 +0200

    added test for prepare_sample_dataset

--- src/common/evaluation/QA/eval_depthmap_models/src/test_evaluate.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/test_evaluate.py
@@ -1,15 +1,19 @@
+#from src.common.evaluation.QA.eval_depthmap_models.src.evaluate import prepare_sample_dataset
 import sys
 import shutil
 from pathlib import Path
 
-from evaluate import copy_dir
+import pandas as pd
 
 here = Path(__file__).resolve()  # not sure if this is best practice - find out
 
-sys.path.append(str(here.parents[1])) # noqa
+sys.path.append(str(here.parents[4]))
 
 REPO_DIR = str(here.parents[6].absolute())
 
+from evaluate import copy_dir  # noqa: E402
+from evaluate import prepare_sample_dataset  # noqa: E402
+#from evaluation.eval_utilities import get_column_list  # noqa: E402
 
 def test_copy_dir():
     common_dir_path = Path(REPO_DIR + "/src/common")
@@ -43,6 +47,158 @@ def test_copy_empty_dir():
         shutil.rmtree(temp_empty_dir)
     except OSError as e:
         print("Error: %s : %s" % (temp_empty_dir, e.strerror))
+
+
+def test_prepare_sample_dataset():
+    print("Entering test_prepare_sample_dataset")
+    # prepare sample dataset
+    # for datasetpath, use cgm-ml/data/anon-depthmap-mini/scans dataset_path = str(REPO_DIR / "data" / dataset_name)
+    dataset_path = str(REPO_DIR + "/data/anon-depthmap-mini")
+    print("datasetpath", dataset_path)
+
+    #prepare df
+    qrcode_list = [
+        "1585004725-18cqo1np0j",
+        "1585004725-18cqo1np0j", 
+        "1585004725-18cqo1np0j",
+        "1585004725-18cqo1np0j",
+        "1585004725-18cqo1np0j", 
+        "1585012629-ac1ippx2qy",
+        "1585012629-ac1ippx2qy",
+        "1585012629-ac1ippx2qy",
+        "1585012629-ac1ippx2qy",
+        "1585012629-ac1ippx2qy",
+        "1585012645-ii2eyvdpib",
+        "1585012645-ii2eyvdpib",
+        "1585012645-ii2eyvdpib",
+        "1585012645-ii2eyvdpib",
+        "1585012645-ii2eyvdpib",
+        ]
+
+    scantype_list = [
+        "100",  # 1st qrcode
+        "100",
+        "100",
+        "101",
+        "102",
+        "100",  # 2nd qrcode
+        "101",
+        "101",
+        "102",
+        "102",
+        "100",  # 3rd qrcode
+        "100",
+        "101",
+        "102",
+        "102",
+        ]
+
+    artifact_list = [
+        "pc_1585004725-18cqo1np0j_1592801845251_100_000.p",  # 100 1st qrcode
+        "pc_1585004725-18cqo1np0j_1592801845251_100_001.p",  # 100
+        "pc_1585004725-18cqo1np0j_1592801845251_100_002.p",  # 100
+        "pc_1585004725-18cqo1np0j_1592801845251_101_000.p",  # 101
+        "pc_1585004725-18cqo1np0j_1592801845251_102_000.p",  # 102
+        "pc_1585012629-ac1ippx2qy_1591848606827_100_000.p",  # 100 2nd qrcode
+        "pc_1585012629-ac1ippx2qy_1591848606827_101_014.p",  # 101
+        "pc_1585012629-ac1ippx2qy_1591848606827_101_015.p",  # 101
+        "pc_1585012629-ac1ippx2qy_1591848606827_102_071.p",  # 102
+        "pc_1585012629-ac1ippx2qy_1591848606827_102_072.p",  # 102
+        "pc_1585012645-ii2eyvdpib_1591848619397_100_000.p",  # 100 3rd qrcode
+        "pc_1585012645-ii2eyvdpib_1591848619397_100_002.p",  # 100
+        "pc_1585012645-ii2eyvdpib_1591848619397_101_008.p",  # 101
+        "pc_1585012645-ii2eyvdpib_1591848619397_102_045.p",  # 102
+        "pc_1585012645-ii2eyvdpib_1591848619397_102_046.p",  # 102
+        ]
+
+    prediction_list = [
+        96.8,
+        96.0,
+        95.8,
+        94.5,
+        96.8,
+        85.3,
+        84.9,
+        85.0,
+        85.2,
+        84.8,
+        79.1,
+        78.8,
+        79.0,
+        79.3,
+        79.6,
+    ]
+
+    target_list = [
+        95.5,
+        95.5,
+        95.5,
+        95.5,
+        95.5,
+        85.0,
+        85.0,
+        85.0,
+        85.0,
+        85.0,
+        79.1,
+        79.1,
+        79.1,
+        79.1,
+        79.1,
+    ]
+
+    COLUMNS=['qrcode', 'artifact', 'scantype', 'GT', 'predicted']
+
+    df = pd.DataFrame({
+        'qrcode': qrcode_list,
+        'artifact': artifact_list,
+        'scantype': scantype_list,
+        'GT': target_list,
+        'predicted': prediction_list
+    }, columns=COLUMNS)
+
+    df_sample = df.groupby(['qrcode', 'scantype']).apply(lambda x: x.sample(1))
+
+    dataset_sample = prepare_sample_dataset(df_sample, dataset_path)
+    print(dataset_sample)
+
+    print("Exiting test_prepare_sample_dataset")
+
+    assert (len(dataset_sample) != 0)
+
+
+test_prepare_sample_dataset()
+
+# so wird df gemacht
+"""      qrcode_list, scantype_list, artifact_list, prediction_list, target_list = get_column_list(
+            new_paths_evaluation, prediction_list_one, DATA_CONFIG, FILTER_CONFIG)"""
+"""df = pd.DataFrame({
+            'qrcode': qrcode_list,
+            'artifact': artifact_list,
+            'scantype': scantype_list,
+            'GT': target_list if target_list[0].shape == tuple() else [el[0] for el in target_list],
+            'predicted': prediction_list
+        }, columns=RESULT_CONFIG.COLUMNS)"""
+# columns=RESULT_CONFIG.COLUMNS --> from qa_config_height_clean: COLUMNS=['qrcode', 'artifact', 'scantype', 'GT', 'predicted'],
+
+# so ist die funktion definiert
+"""def prepare_sample_dataset(df_sample, dataset_path):
+    df_sample['artifact_path'] = df_sample.apply(
+        lambda x: f"{dataset_path}/{x['qrcode']}/{x['scantype']}/{x['artifact']}", axis=1)
+    paths_evaluation = list(df_sample['artifact_path'])
+    dataset_sample = tf.data.Dataset.from_tensor_slices(paths_evaluation)
+    dataset_sample = dataset_sample.map(lambda path: tf_load_pickle(path, DATA_CONFIG.NORMALIZATION_VALUE))
+    dataset_sample = dataset_sample.map(lambda _path, depthmap, targets: (depthmap, targets))
+    dataset_sample = dataset_sample.cache()
+    dataset_sample = dataset_sample.prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset_sample"""
+
+# so wirds aufgerufen
+"""      # Sample one artifact per scan (qrcode, scantype combination)
+        df_sample = df.groupby(['qrcode', 'scantype']).apply(lambda x: x.sample(1))
+
+        # Prepare uncertainty prediction on these artifacts
+        dataset_sample = prepare_sample_dataset(df_sample, dataset_path)"""
 
 
 def test_always_passes():
