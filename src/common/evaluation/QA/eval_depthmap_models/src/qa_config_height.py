8d8224d4e706c34b1bb2bc42bba5b259704fc9ef
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-07-02 09:28:09 +0200

    Change model location for one config, run less configs to speed up debugging

--- src/common/evaluation/QA/eval_depthmap_models/src/qa_config_height.py
+++ src/common/evaluation/QA/eval_depthmap_models/src/qa_config_height.py
@@ -8,7 +8,7 @@ CONFIG_NAME = os.path.splitext(os.path.basename(__file__))[0]
 MODEL_CONFIG = Bunch(dict(
     EXPERIMENT_NAME='q3-depthmap-plaincnn-height-95k',
     RUN_ID='q3-depthmap-plaincnn-height-95k_1617983539_763a24b9',  # Run 9
-    INPUT_LOCATION='outputs',
+    INPUT_LOCATION='model_location',
     NAME='best_model.ckpt',
 ))
 
