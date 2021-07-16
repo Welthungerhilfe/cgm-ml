6ba5e84b5b1f062d6ca6e142c65605d48c1deb0a
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-07-16 16:52:17 +0200

    Skip ensemble config

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -5,7 +5,7 @@ set -euox pipefail
 # python eval_main.py --qa_config_module qa_config_height
 # python eval_main.py --qa_config_module qa_config_height_clean
 # python eval_main.py --qa_config_module qa_config_height_dropout
-python eval_main.py --qa_config_module qa_config_height_deep_ensemble
+# python eval_main.py --qa_config_module qa_config_height_deep_ensemble
 python eval_main.py --qa_config_module qa_config_height_mcnn
 # python eval_main.py --qa_config_module qa_config_height_filter
 
