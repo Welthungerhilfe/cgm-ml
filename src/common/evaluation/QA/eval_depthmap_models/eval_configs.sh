e584c821030a45c6a97a6d1eaf6954d3dbc24f5d
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-07-16 17:56:52 +0200

    Add tags with config name to be able to distinguish the QA pipeline runs in AzureML

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -6,7 +6,7 @@ set -euox pipefail
 # python eval_main.py --qa_config_module qa_config_height_clean
 # python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_height_deep_ensemble  # takes 50min in CI
-python eval_main.py --qa_config_module qa_config_height_mcnn
+python eval_main.py --qa_config_module qa_config_height_mcnn  # takes 19min in CI
 # python eval_main.py --qa_config_module qa_config_height_filter
 
 python eval_main.py --qa_config_module qa_config_weight
