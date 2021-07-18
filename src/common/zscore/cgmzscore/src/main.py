43e481b8e5ddbe55ae8617fa7ca8464a82ac5e2e
prajurock <prajwalsingh651@gmail.com>
2021-07-18 11:24:59 +0530

    rename value

--- src/common/zscore/cgmzscore/src/main.py
+++ src/common/zscore/cgmzscore/src/main.py
@@ -1,6 +1,6 @@
 from constant import REPO_DIR, WHO_tables
 from table import Table, ReformatTable
-from Observation import Value
+from extract_value import Value
 from calculate import Zscore
 from decimal import Decimal as D
 import json
@@ -8,22 +8,21 @@ import json
 
 TABLE_REPO = REPO_DIR / 'tables'
 
-test = {}
+all_json_value = {}
 for table in WHO_tables:
     value = Table(TABLE_REPO / table).loadTable()
-    table_name, underscore, zscore_part = table.split('.')[0].rpartition('_')
+    table_name, underscore, z_score_part = table.split('.')[0].rpartition('_')
     reformatTable = ReformatTable(value)
     new_value = reformatTable.add_value()
     new_dict = reformatTable.append_value(new_value, new_value['field_name'])
-    test[table_name] = new_dict
+    all_json_value[table_name] = new_dict
 
 
 def z_score_calculation(table):
     table_name = table.resolve_table()
-    value = table.get_values(table_name, test)
+    value = table.get_values(table_name, all_json_value)
     skew, median, coff, y = table.resolve_value(value)
-    zscore = Zscore(skew, median, coff, y)
-    return zscore.z_score_measurement()
+    return Zscore(skew, median, coff, y).z_score_measurement()
 
 
 def zScore_wfa(weight=None, muac=None, age_in_days=None, sex=None, height=None):
@@ -50,9 +49,8 @@ def zScore_lhfa(weight=None, muac=None, age_in_days=None, sex=None, height=None)
     return z_score_calculation(table)
 
 
-def zScore_withclass(weight=None, muac=None, age_in_days=None, sex=None, height=None):
-    wfa = zScore_wfa(
-        weight=weight, age_in_days=age_in_days, sex=sex)
+def z_score_with_class(weight=None, muac=None, age_in_days=None, sex=None, height=None):
+    wfa = zScore_wfa(weight=weight, age_in_days=age_in_days, sex=sex)
     if wfa < -3:
         class_wfa = 'Severely Under-weight'
     elif wfa >= -3 and wfa < -2:
@@ -63,17 +61,14 @@ def zScore_withclass(weight=None, muac=None, age_in_days=None, sex=None, height=
     if D(age_in_days) > 24:
         dummy = 'Z_score_WFH'
         dummy_class = "Class_WFH"
-        wfl = zScore_wfh(
-            weight=weight, age_in_days=age_in_days, sex=sex, height=height)
+        wfl = zScore_wfh(weight=weight, age_in_days=age_in_days, sex=sex, height=height)
     else:
         dummy = 'Z_score_WFL'
         dummy_class = "Class_WFL"
-        wfl = zScore_wfl(
-            weight=weight, age_in_days=age_in_days, sex=sex, height=height)
+        wfl = zScore_wfl(weight=weight, age_in_days=age_in_days, sex=sex, height=height)
     class_wfl = SAM_MAM(weight, muac, age_in_days, sex, height)
 
-    lhfa = zScore_lhfa(
-        age_in_days=age_in_days, sex=sex, height=height)
+    lhfa = zScore_lhfa(age_in_days=age_in_days, sex=sex, height=height)
     if lhfa < -3:
         class_lhfa = 'Severely Stunted'
     elif lhfa >= -3 and lhfa < -2:
@@ -81,9 +76,9 @@ def zScore_withclass(weight=None, muac=None, age_in_days=None, sex=None, height=
     else:
         class_lhfa = 'Healthy'
 
-    zscore = json.dumps({'Z_score_WFA': wfa, 'Class_WFA': class_wfa, dummy: wfl,
-                         dummy_class: class_wfl, 'Z_score_HFA': lhfa, 'Class_HFA': class_lhfa})
-    return zscore
+    value = json.dumps({'Z_score_WFA': wfa, 'Class_WFA': class_wfa, dummy: wfl,
+                        dummy_class: class_wfl, 'Z_score_HFA': lhfa, 'Class_HFA': class_lhfa})
+    return value
 
 
 def SAM_MAM(weight=None, muac=None, age_in_days=None, sex=None, height=None):
